e65ced2b13f785d33ba0c4849970b30363b3f7f8
zobaaer <ahmd@zhaw.ch>
2025-10-03 20:34:49 +0200

    Update allow_websocket_8080.bat

--- WebControl/allow_websocket_8080.bat
+++ WebControl/allow_websocket_8080.bat
@@ -2,6 +2,7 @@
 REM Adds a Windows Firewall rule to allow inbound TCP traffic on port 8080 for Unity WebSocket
 
 netsh advfirewall firewall add rule name="Unity WebSocket 8080" dir=in action=allow protocol=TCP localport=8080 profile=any
+netsh advfirewall firewall add rule name="Unity WebSocket 8080" dir=out action=allow protocol=TCP localport=8080 profile=any
 
 echo Firewall rule added for port 8080.
 pause
